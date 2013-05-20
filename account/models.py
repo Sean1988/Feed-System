44335c10d170070c882c52069240394355887500
wenzhi <wenzhixue@gmail.com>
2013-05-20 01:33:09 +0000

    change account field name

--- account/models.py
+++ account/models.py
@@ -73,7 +73,7 @@ class MyUser(AbstractBaseUser,PermissionsMixin):
     idsyAlert = models.BooleanField(default=True)
     momAlert = models.BooleanField(default=True)
     trackSug = models.BooleanField(default=True)
-    feedTotorial= models.BooleanField(default=True) # tutoring for first time using
+    feedTutorial = models.BooleanField(default=True) # tutoring for first time using
 
 
     objects = MyUserManager()
