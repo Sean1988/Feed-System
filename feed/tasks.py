116d40104bd41f9223f0b4ebad5d69c10bd6302a
wenzhi <wenzhixue@gmail.com>
2013-05-21 17:12:10 +0000

    fix release account

--- feed/tasks.py
+++ feed/tasks.py
@@ -3,11 +3,13 @@ from company.models import Company
 from feed.news_fetch import * 
 import time
 from ec2.api import * 
+from scheduler.views import releaseAllAccounts
 
 @task()
 def shutdown(ec2):
     print "shutting down"
     time.sleep(300)
+    releaseAllAccounts()
     ec2.stopAllInstances()
     ec2.cancelAllRequest()
     print "finised"
@@ -15,6 +17,6 @@ def shutdown(ec2):
     
 def updatNewsDaily():
     c = Ec2()
-    c.launchSpotInstance(7,'single_worker')
+    c.launchSpotInstance(10,'single_worker')
     companyList = Company.objects.filter(analysed=True)
     chord( [fetchNewsFromFaroo.delay(item) for item in companyList ])(shutdown.delay(c)).get()
